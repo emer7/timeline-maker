f2016bac73cfffb3cc7a5c963d81c9db260b303e
Gilbert Emerson <emer7@users.noreply.github.com>
2021-10-15 21:38:12 +0800

    Change white black font algorithm

--- utils/color.js
+++ utils/color.js
@@ -1,20 +1,11 @@
 import chroma from 'chroma-js';
 
-const partialLuminance = value =>
-  value <= 0.03928 ? value / 12.92 : Math.pow((value + 0.055) / 1.055, 2.4);
-
-export const getRelativeLuminance = hex => {
-  // https://www.w3.org/TR/WCAG20/#relativeluminancedef
+const getLuminance = hex => {
+  // https://www.nbdtech.com/Blog/archive/2008/04/27/Calculating-the-Perceived-Brightness-of-a-Color.aspx
   const [red, green, blue] = chroma(hex).rgb(false);
 
-  return (
-    0.2126 * partialLuminance(red / 255) +
-    0.7152 * partialLuminance(green / 255) +
-    0.0722 * partialLuminance(blue / 255)
-  );
+  return Math.sqrt(red ** 2 * 0.241 + green ** 2 * 0.691 + blue ** 2 * 0.068);
 };
 
 export const getFontWhiteOrBlack = backgroundHex =>
-  // Contrast is identical at 4.583, for both white and black font
-  // on background with relative luminance of 0.179
-  getRelativeLuminance(backgroundHex) < 0.179 ? 'white' : 'black';
+  getLuminance(backgroundHex) < 130 ? 'white' : 'black';
